1d5972c6d82eb957f8ce4bfe9535335009c475de
Katrin <153351347+kate8kate@users.noreply.github.com>
2023-12-12 20:27:29 +0100

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,51 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	/* C Методами
-Допустим, у вас есть прибор, у которого есть 3 колбы.
-Каждая колба имеет датчик температуры (в программе, соответственно 3 переменных int с температурой).
-Считается, что прибор работает правильно, если температура в каждой из трех колб не меньше 100 или есть такая колба, температура которой более 200 градусов.
-Напишите программу проверки корректности работы такого прибора.
-
- */
-        int temperature1 = 130;
-        int temperature2 = 150;
-        int temperature3 = 101;
-
-        boolean isDeviceOk = checkDevice(temperature1,temperature2,temperature3);
-        printDeviceStatus(temperature1,temperature2,temperature3,isDeviceOk);
+        System.out.println("hello world!");
+        System.out.println("hello from java world!");
+        System.out.println(25+16-7) ;
+        String str = "Andrey";
+        String hero = " Belov";
+        System.out.println("my name is " + str + hero) ;
     }
-
-    public static void printDeviceStatus(int temperature1, int temperature2, int temperature3, boolean isDeviceOk){
-        System.out.println("flask1="+temperature1);
-        System.out.println("flask2="+temperature2);
-        System.out.println("flask3="+temperature3);
-        if (isDeviceOk) {
-            System.out.println(">>>   device is ok!!!");
-        } else {
-            System.out.println(">>>   alarm!!!");
-        }
-    }
-
-
-    /*
-        если температура в каждой из трех колб не меньше 100
-     */
-    public static boolean checkCondition1(int temperature1, int temperature2, int temperature3){
-        return temperature1 >= 100 && temperature2 >= 100 && temperature3 >= 100;
-    }
-
-    /*
-    температура которой более 200 градусов
-     */
-    public  static boolean checkCondition2(int temperature1, int temperature2, int temperature3){
-        return temperature1 > 200 || temperature2 > 200 || temperature3 > 200;
-    }
-
-
-    public  static boolean checkDevice(int temperature1, int temperature2, int temperature3) {
-        return  checkCondition1(temperature1,temperature2,temperature3) ||
-                checkCondition2(temperature1,temperature2,temperature3);
-    }
-    }
-
+}
