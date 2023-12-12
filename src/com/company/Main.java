f0f583e0441e83b3fe4045a7e10438f56c9730ac
Katrin <153351347+kate8kate@users.noreply.github.com>
2023-12-12 20:24:38 +0100

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,34 +3,51 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+	/* C Методами
+Допустим, у вас есть прибор, у которого есть 3 колбы.
+Каждая колба имеет датчик температуры (в программе, соответственно 3 переменных int с температурой).
+Считается, что прибор работает правильно, если температура в каждой из трех колб не меньше 100 или есть такая колба, температура которой более 200 градусов.
+Напишите программу проверки корректности работы такого прибора.
 
-        MyDate date1 = new MyDate(1,5,2005);
+ */
+        int temperature1 = 130;
+        int temperature2 = 150;
+        int temperature3 = 101;
 
-        MyDate date2 = new MyDate(5,10,2015);
+        boolean isDeviceOk = checkDevice(temperature1,temperature2,temperature3);
+        printDeviceStatus(temperature1,temperature2,temperature3,isDeviceOk);
+    }
 
-        MyDate date3 = new MyDate(1,11,2017);
-
-        MyDate [] dates = {date1, date2, date3};
-
-       for (MyDate date : dates){
-           System.out.println(date.toString());
-           
-       }
-
-       Person person1 = new Person("Jack", "Smith", date1);
-       Person person2 = new Person("Egor", "Ivanov", date2);
-       Person person3 = new Person ("Lena", "Smith", date3);
-
-       Person [] people = {person1, person2, person3};
-
-        for (Person person : people){
-
-            System.out.println(person.toString());
-
+    public static void printDeviceStatus(int temperature1, int temperature2, int temperature3, boolean isDeviceOk){
+        System.out.println("flask1="+temperature1);
+        System.out.println("flask2="+temperature2);
+        System.out.println("flask3="+temperature3);
+        if (isDeviceOk) {
+            System.out.println(">>>   device is ok!!!");
+        } else {
+            System.out.println(">>>   alarm!!!");
         }
-
     }
 
+
+    /*
+        если температура в каждой из трех колб не меньше 100
+     */
+    public static boolean checkCondition1(int temperature1, int temperature2, int temperature3){
+        return temperature1 >= 100 && temperature2 >= 100 && temperature3 >= 100;
     }
 
+    /*
+    температура которой более 200 градусов
+     */
+    public  static boolean checkCondition2(int temperature1, int temperature2, int temperature3){
+        return temperature1 > 200 || temperature2 > 200 || temperature3 > 200;
+    }
+
+
+    public  static boolean checkDevice(int temperature1, int temperature2, int temperature3) {
+        return  checkCondition1(temperature1,temperature2,temperature3) ||
+                checkCondition2(temperature1,temperature2,temperature3);
+    }
+    }
 
