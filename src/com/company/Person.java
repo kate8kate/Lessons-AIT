a8adacea360122afb8431aa554195b2da8666618
Katrin <153351347+kate8kate@users.noreply.github.com>
2023-12-12 20:14:13 +0100

    Lesson26_12/2023

---
+++ src/com/company/Person.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Person {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
