c752b91f368892838f2c5e99d7a936a84130a858
Katrin <153351347+kate8kate@users.noreply.github.com>
2023-12-12 20:21:30 +0100

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,40 @@ public class Main {
 
     public static void main(String[] args) {
 
-      People people = new People();
-      people.add(new Person("Jack1"));
-      people.add(new Person("Jack2"));
-      people.add(new Person("Jack3"));
-        System.out.println(people);
+
+
+        String str1 ="Java world is very interest!";
+        String res1 = str1.substring(5,18);
+        System.out.println( res1 );
+
+
+        res1= str1.substring(0, str1.length()-3);
+        System.out.println(str1);
+        System.out.println(res1);
+        res1= str1.substring(3, str1.length()-3);
+        System.out.println("   " + res1);
+        res1 = str1.substring(str1.length()-3);
+        System.out.println(res1);
+        res1= str1.substring(0,3) + str1.substring(str1.length()-3);
+        System.out.println(res1);
+
+    //    System.out.println("A".repeat(10));
+        String str3="";
+        for(int j=0; j<10; j++){
+            str3 = str3 +"A";
+        }
+        System.out.println(str3);
+        System.out.println("---------------------------------------------");
+        str1="ja";
+        //res1= str1.substring(0, (str1.length()>3 ? str1.length()-3 : str1.length()) );
+        res1= str1.length()>3 ?  str1.substring(0, str1.length()-3) : str1;
+        System.out.println(res1);
+        System.out.println("--------------------------------------------");
+
+
+
 
     }
-}
+
+    }
+
